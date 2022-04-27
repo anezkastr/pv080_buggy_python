b89530cc0a440ddc7b17db618949ea7e0ce5839c
xstrnad2 <104461944+xstrnad2@users.noreply.github.com>
2022-04-27 09:02:27 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -7,29 +7,33 @@ import subprocess
 import flask
 
 # Input injection
-def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
-    subprocess.call(command, shell=True)  # a bad idea!
+#def transcode_file(request, filename):
+  #  command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
+   # subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
-    assert user.is_admin, 'user does not have access'
+#def foo(request, user):
+ #   assert user.is_admin, 'user does not have access'
     # secure code...
 
 
 # Pickles
-class RunBinSh(object):
-    def __reduce__(self):
-        return (subprocess.Popen, (('/bin/sh',),))
+#class RunBinSh(object):
+#    def __reduce__(self):
+#        return (subprocess.Popen, (('/bin/sh',),))
 
 def import_urlib_version(version):
     exec("import urllib%s as urllib" % version)
 
-@app.route('/')
-def index():
-    module = flask.request.args.get("module")
-    import_urlib_version(module)
+#@app.route('/')
+#def index():
+ #   module = flask.request.args.get("module")
+  #  import_urlib_version(module)
 
 
-print(base64.b64encode(pickle.dumps(RunBinSh())))
+#print(base64.b64encode(pickle.dumps(RunBinSh())))
+
+if __name__ = '__main__':
+    string = "__import__('os').system.listdir()"
+    import_urlib_version(string)
